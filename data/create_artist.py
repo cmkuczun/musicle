62b2221ec973c27052df33d1b8b1fd27b9fc6d93
cmkuczun <93489352+cmkuczun@users.noreply.github.com>
2024-04-05 13:53:15 -0400

    had to add artist id SILLY ME

--- data/create_artist.py
+++ data/create_artist.py
@@ -1,21 +1,41 @@
 import pandas as pd
 import ast
 
+# read all songs CSV file
 df = pd.read_csv('songs.csv')
 
-# convert string to list
+# convert string representation of list to actual list for 'artists' and 'artist_ids' cols
 df['artists'] = df['artists'].apply(lambda x: ast.literal_eval(x))
+df['artist_ids'] = df['artist_ids'].apply(lambda x: ast.literal_eval(x))
 
-# expand artist arrays
-df = df.explode('artists')
+# initialize empty df to store exploded rows
+exploded_rows = []
 
-# convert type of artist back to string!
-df['artists'] = df['artists'].astype(str)
-df = df.rename(columns={'artists': 'artist'})
+#explore df based on artists/artist_ids
+for _, row in df.iterrows():
+    artists = row['artists']
+    artist_ids = row['artist_ids']
+    # multiple IDs for one artist
+    if len(artists) != len(artist_ids):
+       # unmatching number of artists and artist IDs, create a row for each artist
+        num_artists = min(len(artists), len(artist_ids))
+        for i in range(num_artists):
+            exploded_rows.append({
+                'id': row['id'],
+                'artist': artists[i],
+                'artist_id': artist_ids[i]
+            })
+    else:
+        # matching number of artists and artist IDs, create a row for each artist
+        for artist, artist_id in zip(artists, artist_ids):
+            exploded_rows.append({
+                'id': row['id'],
+                'artist': artist,
+                'artist_id': artist_id
+            })
 
-# keep specified columns
-columns_to_keep = ['id', 'artist']
-df = df[columns_to_keep]
+# create df from exploded rows
+exploded_df = pd.DataFrame(exploded_rows)
 
-# save modified DataFrame back to CSV file
-df.to_csv('artists.csv', index=False)
+# save df to new CSV file
+exploded_df.to_csv('artists.csv', index=False)
