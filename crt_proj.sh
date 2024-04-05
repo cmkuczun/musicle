6e7a3037e2a78068c5462e2aa38a6370793d711f
cmkuczun <93489352+cmkuczun@users.noreply.github.com>
2024-04-05 15:46:43 -0400

    fk constraints in ctl

--- crt_proj.sh
+++
@@ -1,3 +0,0 @@
-sqlldr ckuczun/claud control=sql/song.ctl
-sqlldr ckuczun/claud control=sql/song_artist.ctl
-sqlldr ckuczun/claud control=sql/song_genre.ctl
--- sql/build.sh
+++ sql/build.sh
@@ -17,16 +17,39 @@
 
 # LOAD DATA==========================================================
 # song
+
 sqlldr ckuczun/claud control=sql/song.ctl
+
 # artist
 sqlldr ckuczun/claud control=sql/artist.ctl
 # genre
 sqlldr ckuczun/claud control=sql/genre.ctl
 # album
 sqlldr ckuczun/claud control=sql/album.ctl
+
 # song_artist
+sqlplus ckuczun/claud <<EOF 
+ALTER TABLE song_artist DISABLE CONSTRAINT s_id_fk; ALTER TABLE song_artist DISABLE CONSTRAINT a_id_fk; exit; 
+EOF
 sqlldr ckuczun/claud control=sql/song_artist.ctl
+sqlplus ckuczun/claud <<EOF 
+ALTER TABLE song_artist ENABLE CONSTRAINT s_id_fk; ALTER TABLE song_artist ENABLE CONSTRAINT a_id_fk; exit; 
+EOF
+
 # song_album
+sqlplus ckuczun/claud <<EOF 
+ALTER TABLE song_album DISABLE CONSTRAINT s_id_fk; ALTER TABLE song_album DISABLE CONSTRAINT a_id_fk; exit; 
+EOF
 sqlldr ckuczun/claud control=sql/song_album.ctl
+sqlplus ckuczun/claud <<EOF 
+ALTER TABLE song_album ENABLE CONSTRAINT s_id_fk; ALTER TABLE song_album ENABLE CONSTRAINT a_id_fk; exit; 
+EOF
+
 # artist_genre
+sqlplus ckuczun/claud <<EOF 
+ALTER TABLE artist_genre DISABLE CONSTRAINT a_id_fk; ALTER TABLE artist_genre DISABLE CONSTRAINT g_id_fk; exit; 
+EOF
 sqlldr ckuczun/claud control=sql/artist_genre.ctl
+sqlplus ckuczun/claud <<EOF 
+ALTER TABLE artist_genre ENABLE CONSTRAINT a_id_fk; ALTER TABLE artist_genre ENABLE CONSTRAINT g_id_fk; exit; 
+EOF
