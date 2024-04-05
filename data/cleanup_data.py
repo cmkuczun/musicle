f07bc06e3f3c1be8e86c472fb9edcdc2bf8b630d
cmkuczun <93489352+cmkuczun@users.noreply.github.com>
2024-04-05 16:29:59 -0400

    remade songs for only desired fields

--- data/cleanup_data.py
+++ data/cleanup_data.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-# Read the CSV file
+# read CSV file
 df = pd.read_csv('tracks_features.csv')
 
-# Drop the specified columns
+# drop specified columns
 columns_to_remove = ['track_number', 'disc_number', 'explicit', 'key', 'duration_ms', 'time_signature', 'year']
 df = df.drop(columns=columns_to_remove)
 
-# Save the modified DataFrame back to a CSV file
-df.to_csv('songs.csv', index=False)
+# save modified df back to a CSV file
+df.to_csv('og_songs.csv', index=False)
---
+++ data/make_songs.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# read CSV file
+df = pd.read_csv('og_songs.csv')
+
+# drop specified columns
+columns_to_remove = ['album', 'album_id', 'artists', 'artist_ids', 'release_date']
+df = df.drop(columns=columns_to_remove)
+
+# save modified df back to a CSV file
+df.to_csv('songs.csv', index=False)
