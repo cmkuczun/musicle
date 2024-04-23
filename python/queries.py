6f132f6419ea43a4000a65f42af77f54d9df68ce
cmkuczun <ckuczun@nd.edu>
2024-04-23 15:54:09 +0000

    more queries & tests

--- python/queries.py
+++ python/queries.py
@@ -1,5 +1,6 @@
 import cx_Oracle
 import requests
+from song import Song
 
 # ORACLE DB SETUP====================================================================================================
 # create connection
@@ -198,61 +199,133 @@ def insert_artist(*args):
 
 
 
-# SEARCH QUERIES=========================================================================================================
+# BASIC QUERIES=========================================================================================================
 # build query strings to search for specific data
 
-def search_song_using_id(song_id):
-    # song_id is a string
-    q = 'select * from song where song_id="' + song_id + '"' 
-    return q
+# NOTE MAYBE RETURN TARGET QUERY RESULTS (FOR HINTS) BELOW??? OR IN OTHER FUNC - GET_SONG_STATS.... hmmmm
 
-def search_artist_using_id(artist_id):
-    # artist_id is a string
-    q = 'select * from artist where artist_id="' + artist_id + '"' 
-    return q
+def get_id_from_song(song_name):
+    '''search for song id using EXACT song name'''
+    q = "select song_id from song where song_name like '{}'".format(song_name) 
+    res = execute(q)
+    # handle multiple songs
+    if len(res) > 1:
+        res_arr = []
+        for song in res:
+            res_arr.append(song[0])
+        return res_arr
+    return res[0][0]
 
-def search_genre_using_id(genre_id):
-    # genre_id is a number
-    genre_id=str(genre_id)
-    q = "select * from genre where genre_id=" + genre_id
-    return q
+def get_song_info_from_id(song_id):
+    '''get song characteristics using song id'''
+    q = "select danceability,energy,loudness,song_mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo from song where song_id like '{}'".format(song_id) 
+    res = execute(q)
+    return list(res[0])
 
-def search_album_using_id(album_id):
-    # album_id is a string
-    q = 'select * from album where album_id="' + album_id + '";' 
-    return q
-
-def search_user_using_id(user_id):
-    # user_id is a number
-    q = 'select * from user where user_id=' + user_id + ';' 
-    return q
-
-
-def get_song_artist_id(song_id):
-    '''get artist id(s) from song id'''
-    artist_id_arr = []
-    #TODO
-    return artist_id_arr
-
-def get_artist_genre_id(artist_id):
-    '''get genre id(s) from artist id'''
-    genre_id_arr = []
-    #TODO
-    return genre_id_arr
-
-def get_song_album_id(song_id):
+def get_album_from_song(song_id):
     '''get album id from song id'''
-    album_id_arr = []
-    #TODO
-    return album_id_arr
+    q = "select album_id from song_album where song_id='{}'".format(song_id)
+    res = execute(q)
+    return res[0][0]
 
-def get_song_stats(song_name):
+def get_album_from_id(album_id):
+    '''get album info from album_id'''
+    q = "select album_name,release_date from album where album_id='{}'".format(album_id)
+    res = execute(q)
+    return list(res[0])
+
+def get_artist_from_song(song_id):
+    '''get artist id from song id'''
+    q = "select artist_id from song_artist where song_id='{}'".format(song_id)
+    res = execute(q)
+    # handle multiple artists
+    if len(res) > 1:
+        res_arr = []
+        for artist in res:
+            res_arr.append(artist[0])
+        return res_arr
+    return res[0]
+
+def get_artist_from_id(artist_id):
+    '''get artist name from artist id'''
+    q = "select artist_name from artist where artist_id='{}'".format(artist_id)
+    res = execute(q)
+    return res[0][0]
+
+def get_genre_from_artist(artist_id):
+    '''get genre id from artist id'''
+    q = "select * from artist_genre where artist_id='{}'".format(artist_id)
+    res = execute(q)
+    # handle multiple genres
+    if len(res) == 0:
+        print('ERROR: No genre found.')
+        return []
+    if len(res) > 1:
+        res_arr = []
+        for genre in res:
+            res_arr.append(genre[0])
+        return res_arr
+    return res[0][0]
+
+def get_genre_from_id(genre_id):
+    '''get genre name from genre id'''
+    q = "select genre_name from genre where genre_id='{}'".format(str(genre_id))
+    res = execute(q)
+    return res[0][0]
+
+
+
+# ADVANCED QUERIES=========================================================================================================
+def get_song_stats(target_song_name):
     '''get all information regarding a song, starting with song name'''
+    # get song id from song name
+    target_song_id = get_id_from_song(target_song_name)
+    print(f'song id = {target_song_id}')
 
+    # get song information
+    target_song_info = get_song_info_from_id(target_song_id)
+    print(f'song info = {target_song_info}')
+
+    # get album id
+    target_album_id = get_album_from_song(target_song_id)
+    print(f'album id = {target_album_id}')
+
+    # get album name
+    target_album_name = get_album_from_id(target_album_id)
+    print(f'album info = {target_album_name}')
+
+    # get artist id
+    target_artists = get_artist_from_song(target_song_id)
+    print(f'artist id(s) = {target_artists}')
+
+    # get artist(s) from id(s)
+    target_artist_names = []
+    for a_id in target_artists:
+        res = get_artist_from_id(a_id)
+        target_artist_names.append(res)
+    print(f'artist name(s) = {target_artist_names}')
+
+    # get genre id
+    target_genre_id = []
+    for t in artists:
+        res = get_genre_from_artist(t)
+        if len(res) > 0:
+            target_genre_id.append(res)
+    print(f'genre ids = {target_genre_id}')
+
+    target_genre = []
+    for t in target_genre_id:
+        res = get_genre_from_id(t)
+        target_genre.append(res)
+
+    print(f'genres = {target_genre}')
+
+# TODO: get user guesses for specific puzzle, get all DAILY puzzles (user_id null), get all CUSTOM puzzles (user_id not null)
+# TODO 2: ...
 
 # API QUERY=========================================================================================================
 # query API to get similar artists
-def get_similar_artists(artist_name):
+def get_similar_artists(artist_name):#
     url = f"http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist={artist_name}&api_key={api_key}&format=json"
     
     try:
